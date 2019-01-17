90fdac0a7f3b457e6edee55f6373155ad13fb69f
kamare2 <abdklt6@gmail.com>
2019-01-17 14:45:36 -0600

    myname

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Kaleab Amare Mengistu
+ * Date: 2019/01/17
+ *
+ * Writing my name
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kaleab Mengistu");
+
+  return 0;
+}
